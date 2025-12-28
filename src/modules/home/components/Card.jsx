7766ae221acfbed53e2206d5532d85e833516b7f
edwinalbanez <edwinalbanez26@gmail.com>
2025-12-28 00:14:16 -0600

    chore: install tailwind

---
+++ src/modules/home/components/Card.jsx
@@ -0,0 +1,7 @@
+const Card = () => {
+  return (
+    <div>Card</div>
+  )
+}
+
+export { Card }
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 import { fileURLToPath } from 'url'
 
@@ -7,7 +8,10 @@ const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(dirname, "./src")
