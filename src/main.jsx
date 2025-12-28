36ba78a0168d6a5f05c51039a01d978768b96391
edwinalbanez <edwinalbanez26@gmail.com>
2025-12-28 00:01:41 -0600

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import './index.css';
+import App from "./App";
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { Home } from "@/modules/home/Home";
+import { Contact } from '@/modules/contact/Contact';
+import { AuthLayout, Login } from "@/modules/auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      { index: true, Component: Home },
+      { path: "contact", Component: Contact },
+      {
+        path: "auth",
+        Component: AuthLayout,
+        children: [
+          { path: "login", Component: Login }
+        ],
+      },
+    ],
+  },
+]);
+;
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ src/modules/auth/AuthLayout.jsx
@@ -0,0 +1,7 @@
+const AuthLayout = () => {
+  return (
+    <div>AuthLayout</div>
+  )
+}
+
+export { AuthLayout }
